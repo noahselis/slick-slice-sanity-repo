4e1bc34a8c75a1b1cd276e7b82363253a8cd7e19
Noah Selis <80969784+noahselis@users.noreply.github.com>
2022-12-20 14:56:38 -0700

    wrote some schemas for pizzas and toppings - created relationship betweeen pizza and toppings - wrote custom input for currency conversion

--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,5 +1,7 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
 
+import PriceInput from '../components/PriceInput';
+
 export default {
   // computer name
   name: 'pizza',
@@ -19,7 +21,7 @@ export default {
       title: 'Slug',
       type: 'slug',
       options: {
-        name: 'slug',
+        source: 'name',
         maxLength: 100,
       },
     },
@@ -38,6 +40,34 @@ export default {
       description: 'Price of the pizza in cents',
       validation: (Rule) => Rule.min(1000),
       // TODO: Add custom input component
+      inputComponent: PriceInput,
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      console.log('STAY');
+      // filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // return preview object fro the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
